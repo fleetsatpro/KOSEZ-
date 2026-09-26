597f924555b555fefa385c500473a30b03ef4ec9
fleetsatpro <griphilgaming@gmail.com>
2026-09-26 23:58:52 +0300

    chore: remove obsolete tandem language catalog import

--- src/routes/_app/tandem.tsx
+++ src/routes/_app/tandem.tsx
@@ -7,7 +7,6 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   LEARNER_MEMORY,
-  PRONLAB_SETS,
   setsForLanguage,
   planAllows,
 } from "@/lib/blossom/data";
