96835e099ccf23325f56855a758f622a43aed1e1
fleetsatpro <griphilgaming@gmail.com>
2026-09-26 23:49:55 +0300

    audit: enforce canonical target-language item isolation

--- src/routes/_app/pronlab.$setId.tsx
+++ src/routes/_app/pronlab.$setId.tsx
@@ -106,7 +106,7 @@ function PronlabSetPage() {
         growthEvents,
         phonemeLeaves,
         missionSessions,
-        allItems: PRONLAB_SETS.filter((s) => !s.language || s.language === "English" || s.language === languageId).flatMap((s) => s.items),
+        allItems: setsForLanguage(languageId).flatMap((s) => s.items),
         memory: LEARNER_MEMORY,
         memoryOn: true,
         languageId,
